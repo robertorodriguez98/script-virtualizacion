001b7f15fa51c472900f5423ffae06d939bf66ee
Roberto Rodríguez Márquez <krowno.iso@gmail.com>
2022-11-07 01:25:53 +0100

    modificado el script para que no use sudo

--- script.sh
+++ script.sh
@@ -4,9 +4,11 @@ echo "################################"
 echo "1. Crea una imagen nueva"
 echo "################################"
 
-virsh -c qemu:///system vol-create-as default maquina1.qcow2 5G --format raw
+#virsh -c qemu:///system vol-create-as default maquina1.qcow2 5G --format raw
+## este comando no lo puedo ejecutar sin sudo, el siguiente si
 
-virt-resize --expand /dev/vda1 /home/roberto/bullseye-base.qcow2 /var/lib/libvirt/images/maquina1.qcow2
+qemu-img create -f qcow2 -b bullseye-base.qcow2 maquina1.qcow2 5G
+virt-resize --expand /dev/vda1 bullseye-base.qcow2 maquina1.qcow2
 
 echo "################################"
 echo "2. Crea una red interna"
@@ -23,7 +25,7 @@ echo "################################"
 virt-install --connect qemu:///system \
 			 --virt-type kvm \
 			 --name maquina1 \
-			 --disk /var/lib/libvirt/images/maquina1.qcow2 \
+			 --disk maquina1.qcow2 \
 			 --os-variant debian10 \
 			 --memory 1024 \
 			 --vcpus 1 \
@@ -32,8 +34,8 @@ virt-install --connect qemu:///system \
              --import \
              --noautoconsole
 sleep 20
-IPm1=$(virsh domifaddr maquina1 | egrep -o '(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}')
-ssh -i /home/roberto/clave-ecdsa -o "StrictHostKeyChecking no" debian@$IPm1 'sudo hostnamectl set-hostname maquina1'
+IPm1=$(virsh -c qemu:///system domifaddr maquina1 | egrep -o '(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}')
+ssh -i ~/.ssh/clave-ecdsa -o "StrictHostKeyChecking no" debian@$IPm1 'sudo hostnamectl set-hostname maquina1'
 
 echo "################################"
 echo "4. Crea un volumen adicional de 1 GiB"
@@ -50,4 +52,4 @@ virsh -c qemu:///system  attach-disk maquina1 \
 --target vdb \
 --persistent
 
-ssh -i /home/roberto/clave-ecdsa debian@$IPm1 'sudo mkdir -p /var/www/html && sudo mkfs.xfs /dev/vdb && sudo mount /dev/vdb /var/www/html'
+ssh -i ~/.ssh/clave-ecdsa debian@$IPm1 'sudo mkdir -p /var/www/html && sudo mkfs.xfs /dev/vdb && sudo mount /dev/vdb /var/www/html'
